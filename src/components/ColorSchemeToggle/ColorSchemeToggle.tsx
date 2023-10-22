f9e20c261a3d08ed269dd06c5b8d9f9631070927
Philly McGrath <phillymcgrath@Phils-M2-Pro.local>
2023-10-22 16:07:59 -0700

    Update toggle to change checked boolean and color scheme

--- src/components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ src/components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -2,16 +2,21 @@
 
 import { Group, Switch, useMantineColorScheme } from '@mantine/core';
 import { IconMoon, IconSun } from '@tabler/icons-react';
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 export function ColorSchemeToggle() {
   const { setColorScheme } = useMantineColorScheme();
-  const [checked] = useState(true);
+  const [checked, setChecked] = useState(true);
+
+  const setToggle = (event: ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.currentTarget.checked);
+    setColorScheme(event.currentTarget.checked ? 'dark' : 'light')
+  }
 
   return (
     <Group justify="center" mt="xl">
       {checked ? <IconMoon /> : <IconSun />}
-      <Switch checked={checked} onChange={(event) => setColorScheme(event.currentTarget.checked ? 'dark' : 'light')} />
+      <Switch checked={checked} onChange={(event) => setToggle(event)} />
     </Group>
   );
 }
